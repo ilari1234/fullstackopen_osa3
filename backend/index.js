a5b483a5d04492bd184d330b338704bb109b00c0
Ilari Lehtonen <ilari.lehtonen@gmail.com>
2024-01-02 22:02:01 +0200

    3.13-3.14

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
 
 const unknownEndpoint = (request, response) => {
@@ -18,44 +20,16 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postBody'))
 
-let persons = [
-    {
-        'id':1,
-        'name':'Arto Hellas',
-        'number':'040-123456'
-    },
-    {
-        'id':2,
-        'name':'Ada Lovelace',
-        'number':'39-44-5323523'
-    },
-    {
-        'id':3,
-        'name':'Dan Abramov',
-        'number':'12-43-234345'
-    },
-    {
-        'id':4,
-        'name':'Mary Poppendick',
-        'number':'39-23-6423122'
-    }
-]
-
-const generateId = () =>  Math.floor(Math.random() * 1000000)
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
   })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => id === person.id)
-
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
   })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -80,16 +54,14 @@ app.post('/api/persons', (request, response) => {
           })
     }
 
-    if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
+    const person = new Person ({
+        name: body.name,
+        number: body.number
+    })
 
-    body.id = generateId()
-    persons = persons.concat(body)
-    
-    response.json(body)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
   })
 
 app.get('/info', (request, response) => {
